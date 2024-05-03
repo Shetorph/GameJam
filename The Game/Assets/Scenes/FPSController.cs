08280f14b061858c988622b09edf6a9f10d170c2
Eren Çakır <erencakkr28@gmail.com>
2024-05-03 16:27:01 +0300

    1

--- The Game/Assets/Scenes/FPSController.cs
+++ The Game/Assets/Scenes/FPSController.cs
@@ -1,6 +1,5 @@
 using System.Collections;
 using System.Collections.Generic;
-using Unity.Mathematics;
 using UnityEngine;
 [RequireComponent(typeof(CharacterController))]
 public class FPSController : MonoBehaviour

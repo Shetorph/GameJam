ca6bb602174bb4ec81e9393be4e958c95dcd3d9f
Quitera <yuksekdagemir3392@gmail.com>
2024-05-03 19:25:18 +0300

    interaction tamamdır

--- The Game/Assets/Scenes/Scripts/Interactable.cs
+++ The Game/Assets/Scenes/Scripts/Interactable.cs
@@ -4,10 +4,15 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.Events;
 
-public class Interaction : MonoBehaviour
+public class Interactable : MonoBehaviour
 {
     Outline outline;
     public string message;
 
     public UnityEvent onInteraction;
+
+    public void Interact()
+    {
+        onInteraction.Invoke();
+    }
 }
--- The Game/Assets/Scenes/Scripts/PlayerInteraction.cs
+++ The Game/Assets/Scenes/Scripts/PlayerInteraction.cs
@@ -5,9 +5,55 @@ using UnityEngine;
 public class PlayerInteraction : MonoBehaviour
 {
     public float playerReach = 3f;
+    Interactable currentInteractable;
 
     void Update()
     {
-    
+        CheckInteraction();
+        if(Input.GetKeyDown(KeyCode.F) && currentInteractable != null)
+        {
+            currentInteractable.Interact();
+        }
+    }
+
+    void CheckInteraction()
+    {
+        RaycastHit hit;
+        Ray ray = new Ray(Camera.main.transform.position, Camera.main.transform.forward);
+
+        if(Physics.Raycast(ray,out hit,playerReach))
+        {
+            if(hit.collider.tag == "Interactable")
+            {
+                Interactable newInteractable = hit.collider.GetComponent<Interactable>();
+                if(newInteractable.enabled)
+                {
+                    SetNewCurrentInteractable(newInteractable);
+                }
+            }
+            else
+        {
+            DisableCurrentInteractable();
+        }
+
+        }
+        else
+        {
+            DisableCurrentInteractable();
+        }
+    }
+
+    void SetNewCurrentInteractable(Interactable newInteractable)
+    {
+         currentInteractable = newInteractable;
+         
+    }
+
+    void DisableCurrentInteractable()
+    {
+        if(currentInteractable)
+        {
+            currentInteractable = null;
+        }
     }
 }
